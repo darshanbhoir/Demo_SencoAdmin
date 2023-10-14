7527cbbe253a84f41a26ae1cc75dd4e1773835e0
darsh <darsh@VOSTRO>
2023-10-14 18:31:08 +0530

    14/10 Login work, filters work

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo_Senco_Admin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,26 @@ namespace Demo_Senco_Admin.Controllers
 {
     public class LoginController : Controller
     {
+        private SENCO_DB_AdminEntities db = new SENCO_DB_AdminEntities();
         // GET: Login
         public ActionResult Index()
         {
             return View("Login");
         }
 
-        public ActionResult login()
+        public ActionResult login(string email, string password)
         {
-            return RedirectToAction("Index", "Dashboard");
+            var query = db.tbl_admin_login.SingleOrDefault(u => u.email == email && u.password == password);
+            if(query != null)
+            {
+                Session["AdminName"] = query.name;
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid username or Password";
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
